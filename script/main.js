99d082e2cf264d538c2a199ebede28292ed700e6
pedrofelixg <pedro.viniciusacm@gmail.com>
2023-05-30 09:46:29 -0300

    :white_check_mark: Alert update

--- script/main.js
+++ script/main.js
@@ -2,12 +2,17 @@ const botao = document.querySelector("#btn__atividade");
 
 function getName(){
     const atividade = document.querySelector("#atividade");
-    
-    return `
+    const atividadeVazia = "";
+
+    if(atividade.value === atividadeVazia){
+        alert("É preciso informar uma atividade válida");
+    } else {
+        return `
     <div class="to-do__item">
         <input type="checkbox" class="check">
         <p class="elemento__texto">${atividade.value}</p>
     </div>`
+    }
 }
 
 function printNewWork(){
